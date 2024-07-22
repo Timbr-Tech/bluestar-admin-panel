9dec15e4b0dc2bad1522faf5df14439cdba6af1c
PrathamDogra <dogra.pratham@gmail.com>
2024-07-23 01:55:13 +0530

    ADD: Minor Fixes

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,26 +1,51 @@
 /* eslint-disable */
-import styles from "./index.module.scss";
-import { ReactComponent as Home } from "../../icons/homeLine.svg";
+import { Tooltip } from "antd";
+import { ReactComponent as Dashboard } from "../../icons/homeLine.svg";
 import { ReactComponent as Database } from "../../icons/database.svg";
 import { ReactComponent as Drivers } from "../../icons/drivers.svg";
 import { ReactComponent as BookingsIcon } from "../../icons/bookings.svg";
+import { ReactComponent as VehicleRename } from "../../icons/VehicleRename.svg";
+import { ReactComponent as Availability } from "../../icons/Availability.svg";
+import { ReactComponent as Logo } from "../../icons/TempLogo.svg";
+import styles from "./index.module.scss";
 
 const Sidebar = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
       <div className={styles.iconsContainer}>
-        <div className={styles.icon}>
-          <Home className={styles.svgIcon} />
-        </div>
-        <div className={styles.icon}>
-          <BookingsIcon className={styles.svgIcon} />
-        </div>
-        <div className={styles.icon}>
-          <Database className={styles.svgIcon} />
-        </div>
-        <div className={styles.icon}>
-          <Drivers className={styles.svgIcon} />
-        </div>
+        <Tooltip placement="right" title={"Dashboard"}>
+          <div className={styles.icon}>
+            <Dashboard className={styles.svgIcon} />
+          </div>
+        </Tooltip>
+        <Tooltip placement="right" title={"Bookings"}>
+          <div className={styles.icon}>
+            <BookingsIcon className={styles.svgIcon} />
+          </div>
+        </Tooltip>
+        <Tooltip placement="right" title={"Database"}>
+          <div className={styles.icon}>
+            <Database className={styles.svgIcon} />
+          </div>
+        </Tooltip>
+        <Tooltip placement="right" title={"Drivers attendance & Payroll"}>
+          <div className={styles.icon}>
+            <Drivers className={styles.svgIcon} />
+          </div>
+        </Tooltip>
+        <Tooltip placement="right" title={"Vehicle Expense"}>
+          <div className={styles.icon}>
+            <VehicleRename className={styles.svgIcon} />
+          </div>
+        </Tooltip>
+        <Tooltip placement="right" title={"Availability"}>
+          <div className={styles.icon}>
+            <Availability className={styles.svgIcon} />
+          </div>
+        </Tooltip>
       </div>
     </div>
   );
