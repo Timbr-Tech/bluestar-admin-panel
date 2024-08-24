c5a4d7d8051aad5099915570fd26a950cc874853
PrathamDogra <dogra.pratham@gmail.com>
2024-08-24 23:36:55 +0530

    ADD: Allowances added

--- src/components/DatabaseTable/AllowancesTable/AllowancesForm.tsx
+++ src/components/DatabaseTable/AllowancesTable/AllowancesForm.tsx
@@ -1,7 +1,22 @@
 /* eslint-disable */
+import SecondaryBtn from "../../SecondaryBtn";
+import PrimaryBtn from "../../PrimaryBtn";
+import { Select } from "antd";
+import { useState, useMemo } from "react";
+import { ALLOWANCES_TYPES, Allowance } from "../../../constants/database";
 import styles from "../DutyTypeTable/index.module.scss";
 
 const AllowancesForm = () => {
+  const [allowanceType, setAllowanceType] = useState<string>("");
+
+  const handleAllowanceType = (value: string) => {
+    setAllowanceType(value);
+  };
+
+  const allowanceLabel = useMemo(() => {
+    return ALLOWANCES_TYPES.filter((type) => type.value === allowanceType)[0];
+  }, [allowanceType]);
+
   return (
     <div className={styles.formContainer}>
       <div className={styles.container}>
@@ -9,6 +24,49 @@ const AllowancesForm = () => {
           <div className={styles.header}>New Allowance</div>
           <div className={styles.primaryText}>Redesign of untitledui.com</div>
         </div>
+        <div className={styles.form}>
+          <div className={styles.typeContainer}>
+            <div className={styles.text}>
+              <p>Taxes</p>
+            </div>
+            <Select
+              style={{ width: "100%" }}
+              onChange={handleAllowanceType}
+              placeholder="Select One"
+              dropdownRender={(menu) => <>{menu}</>}
+              options={ALLOWANCES_TYPES.map((type) => ({
+                label: type.label,
+                value: type.value,
+              }))}
+            />
+          </div>
+          {allowanceType && (
+            <div className={styles.allowance}>
+              <p
+                className={styles.allowanceType}
+              >{`${allowanceLabel.label}:`}</p>
+              {Allowance(allowanceType)}
+            </div>
+          )}
+          {allowanceType && (
+            <div className={styles.typeContainer}>
+              <div className={styles.text}>
+                <p>Add Rate (per hour)</p>
+                <sup>*</sup>
+              </div>
+              <input
+                type="number"
+                className={styles.input}
+                placeholder="Enter amount..."
+                defaultValue={500}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className={styles.bottomContainer}>
+        <SecondaryBtn btnText="Cancel" onClick={() => {}} />
+        <PrimaryBtn btnText="Save" onClick={() => {}} />
       </div>
     </div>
   );
--- src/constants/database.ts
+++ src/constants/database.ts
@@ -217,3 +217,67 @@ export const CUSTOMER_TAX_TYPES = [
   },
   { value: "job", label: "Job" },
 ];
+
+export const ALLOWANCES_TYPES = [
+  {
+    value: "dailyAllowance",
+    label: "Daily Allowance",
+  },
+  {
+    value: "overtimeAllowance",
+    label: "Overtime Allowance",
+  },
+  {
+    value: "outstationAllowance",
+    label: "Outstation Allowance",
+  },
+  {
+    value: "outstationOvernightAllowance",
+    label: "Outstation Overnight Allowance",
+  },
+  {
+    value: "offDayAllowance",
+    label: "Off-Day Allowance",
+  },
+  {
+    value: "earlyStartAllowance",
+    label: "Early Start Allowance",
+  },
+  {
+    value: "nightAllowance",
+    label: "Night Allowance",
+  },
+  {
+    value: "extraDutyAllowance",
+    label: "Extra Duty Allowance",
+  },
+  {
+    value: "airportAllowance",
+    label: "Airport Allowance",
+  },
+];
+
+export const Allowance = (value: string) => {
+  switch (value) {
+    case "dailyAllowance":
+      return "Automatically included in the driver's salary for each day of duty, this allowance compensates for regular daily service provided.";
+    case "overtimeAllowance":
+      return "This allowance is provided on an hourly basis when a driver works beyond the standard number of designated hours, ensuring fair compensation for additional time spent on duty.";
+    case "outstationAllowance":
+      return "Automatically applied when a driver’s duty requires travel beyond the standard operating area, this allowance compensates for the additional responsibilities and time associated with outstation duties.";
+    case "outstationOvernightAllowance":
+      return "This allowance is automatically included if an outstation duty extends overnight, covering the driver's time and expenses for duties that require an overnight stay.";
+    case "offDayAllowance":
+      return "Awarded when a driver is required to work on their designated off day, this allowance ensures they are compensated for their additional commitment.";
+    case "earlyStartAllowance":
+      return "Provided on an hourly basis, this allowance compensates drivers who begin their duties earlier than their scheduled start time.";
+    case "nightAllowance":
+      return "Applied for duties that extend beyond a single day, this allowance compensates for the extended service hours and the overnight commitment required from the driver.";
+    case "extraDutyAllowance":
+      return "This allowance is awarded when a driver performs more than one duty within a single day. Each additional duty qualifies the driver for an additional allowance.";
+    case "airportAllowance":
+      return "Specifically added for duties that involve trips to the airport, this allowance compensates for the specific requirements and responsibilities associated with airport-related duties.";
+    default:
+      return null;
+  }
+};
