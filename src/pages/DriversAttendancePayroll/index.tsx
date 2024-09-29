91aa0a91999fccdc47b47317f82e4b7da1919538
Shadab Ali <sidshaab97@gmail.com>
2024-09-29 13:49:10 +0530

    add menu in payroll

--- src/pages/DriversAttendancePayroll/index.tsx
+++ src/pages/DriversAttendancePayroll/index.tsx
@@ -17,11 +17,13 @@ import {
   Tabs,
 } from "antd";
 import CustomPagination from "../../components/Common/Pagination";
-import {
+import Icon, {
   CheckCircleTwoTone,
+  CheckOutlined,
   CopyOutlined,
   EditFilled,
   MoreOutlined,
+  RightOutlined,
   SearchOutlined,
 } from "@ant-design/icons";
 import row from "antd/es/row";
@@ -120,6 +122,53 @@ function ReturnItems(row: any) {
   ];
   return items;
 }
+const RupeeIcon = () => (
+  <span style={{ fontSize: "16px", marginRight: "4px" }}>₹</span>
+);
+function ReturnItemsPayroll(row: any) {
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: (
+        <div onClick={() => {}}>
+          <Space>
+            <RupeeIcon />
+            Edit Driver Payment
+          </Space>
+        </div>
+      ),
+    },
+    {
+      type: "divider",
+    },
+    {
+      key: "2",
+      label: (
+        <div onClick={() => {}}>
+          <Space>
+            <CopyOutlined twoToneColor="#52c41a" />
+            View Expense logs
+          </Space>
+        </div>
+      ),
+    },
+    {
+      type: "divider",
+    },
+    {
+      key: "3",
+      label: (
+        <div onClick={() => {}}>
+          <Space>
+            <CheckOutlined twoToneColor="#52c41a" />
+            Mark as paid
+          </Space>
+        </div>
+      ),
+    },
+  ];
+  return items;
+}
 const columns: TableColumnsType<any> = [
   {
     title: "Full Name",
@@ -187,6 +236,79 @@ const columns: TableColumnsType<any> = [
     render: () => (
       <div>
         <Dropdown menu={{ items: ReturnItems(row) }}>
+          <MoreOutlined />
+        </Dropdown>
+      </div>
+    ),
+  },
+];
+const columnsPayroll: TableColumnsType<any> = [
+  {
+    title: "Full Name",
+    width: 300,
+    dataIndex: "name",
+    key: "name",
+    fixed: "left",
+    render: (value, row) => {
+      return (
+        <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+          <Avatar>{value[0]}</Avatar>
+          {value}
+        </div>
+      );
+    },
+  },
+  {
+    title: "Age",
+    width: 100,
+    dataIndex: "age",
+    key: "age",
+    sorter: true,
+  },
+  {
+    title: "date 1",
+    dataIndex: "attendance",
+    key: "1",
+
+    render: (value, row) => (
+      <div
+        style={{
+          background: value === "P" ? "green" : "red",
+          color: "white",
+          padding: "1rem",
+          margin: "-1rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {value}
+      </div>
+    ),
+  },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+  { title: "date 2", dataIndex: "attendance", key: "2" },
+
+  {
+    title: "Action",
+    key: "operation",
+    fixed: "right",
+    width: 100,
+    render: () => (
+      <div>
+        <Dropdown menu={{ items: ReturnItemsPayroll(row) }}>
           <MoreOutlined />
         </Dropdown>
       </div>
@@ -261,7 +383,7 @@ const DriversAttendancePayroll = () => {
           <TabPane tab="Payroll" key="payroll">
             <Table
               pagination={false}
-              columns={columns}
+              columns={columnsPayroll}
               dataSource={dataSource}
               scroll={{ x: "max-content" }}
               footer={() => (
