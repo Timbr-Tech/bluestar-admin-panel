9507c7a2ff685e76f8313ad51749df8abce43f4f
Shadab Ali <sidshaab97@gmail.com>
2024-10-02 15:18:49 +0530

    ADD duty slip drawer component

--- src/components/Attendance/Table.tsx
+++ src/components/Attendance/Table.tsx
@@ -1,104 +1,26 @@
 /* eslint-disable */
 import {
   Avatar,
-  Button,
-  DatePicker,
   Drawer,
   Dropdown,
-  Flex,
   MenuProps,
-  Popover,
-  Radio,
   Space,
   Table,
   TableColumnsType,
-  Tag,
 } from "antd";
 import CustomPagination from "../Common/Pagination";
-import { CopyOutlined, EditFilled, MoreOutlined } from "@ant-design/icons";
-import row from "antd/es/row";
-import { useState } from "react";
+import { CopyOutlined, MoreOutlined } from "@ant-design/icons";
+
 import {
   setIsViewDrawerClose,
   setIsViewDrawerOpen,
 } from "../../redux/slices/attendanceSlice";
 import { useAppDispatch, useAppSelector } from "../../hooks/store";
 import { RootState } from "../../types/store";
-import { divide } from "lodash";
+import ViewLogs from "./ViewLogs";
 
-const MarkedAttendance = () => {
-  const [open, setOpen] = useState(false);
-  const options = [
-    { label: "Today", value: "today" },
-    { label: "Select Date", value: "selectDate" },
-  ];
-  const hide = () => {
-    setOpen(false);
-  };
+import MarkedAttendance from "./MarkAttendance";
 
-  const handleChange = (value: string) => {
-    console.log(`Selected month: ${value}`);
-  };
-
-  const handleOpenChange = (newOpen: boolean) => {
-    setOpen(newOpen);
-  };
-  return (
-    <Popover
-      content={
-        <div style={{ width: "100%" }}>
-          {/* <a onClick={hide}>Close</a> */}
-          <Flex vertical gap="middle">
-            <Radio.Group
-              options={options}
-              defaultValue="today"
-              optionType="button"
-              buttonStyle="solid"
-              style={{ width: "100%" }}
-            />
-          </Flex>
-          <DatePicker
-            style={{
-              width: "100%",
-              marginTop: "0.5rem",
-              overflow: "scroll",
-            }}
-            multiple
-            allowClear
-            maxTagCount="responsive"
-            onChange={handleChange}
-          />
-          <Flex
-            style={{
-              marginTop: "0.5rem",
-            }}
-            gap="middle"
-          >
-            <Button>Mark Absent</Button>
-            <Button type="primary">Mark Present</Button>
-          </Flex>
-        </div>
-      }
-      title={
-        <div>
-          <p>Marked attendance</p>
-          <small>For Shadab</small>
-        </div>
-      }
-      trigger="click"
-      open={open}
-      arrow={false}
-      onOpenChange={handleOpenChange}
-    >
-      <div>
-        <Space>
-          <EditFilled twoToneColor="#52c41a" />
-          Marked Attendance
-        </Space>
-      </div>
-    </Popover>
-  );
-};
 function ReturnItems(row: any) {
   const dispatch = useAppDispatch();
   const items: MenuProps["items"] = [
@@ -181,7 +103,7 @@ const columns: TableColumnsType<any> = [
     key: "operation",
     fixed: "right",
     width: 100,
-    render: () => (
+    render: (_, row) => (
       <div>
         <Dropdown menu={{ items: ReturnItems(row) }}>
           <MoreOutlined />
@@ -232,96 +154,7 @@ const AttendanceTable = () => {
         width={"700px"}
         open={isViewDrawerOpen}
       >
-        {[1, 2, 3, 4].map(() => (
-          <div>
-            <h4
-              style={{
-                margin: "1rem 0rem",
-              }}
-            >
-              Aug 28, 2024
-            </h4>
-            <Table
-              showHeader={false}
-              bordered
-              pagination={false}
-              columns={[
-                {
-                  dataIndex: "id",
-                  key: "id",
-                  render: (value: any, row: any) => {
-                    return (
-                      <div>
-                        <p>{value}</p>
-                        <p>{row["time"]}</p>
-                      </div>
-                    );
-                  },
-                },
-                {
-                  dataIndex: "vehicleName",
-                  key: "vehicleName",
-                  render: (value: any, row: any) => {
-                    return (
-                      <div>
-                        <p>{value}</p>
-                        <p>{row["vehicleNumber"]}</p>
-                      </div>
-                    );
-                  },
-                },
-                {
-                  dataIndex: "status",
-                  key: "status",
-                  render: (value) => {
-                    if (value === "alloted") {
-                      return <Tag color="yellow">{value}</Tag>;
-                    } else if (value === "completed") {
-                      return <Tag color="green">{value}</Tag>;
-                    } else {
-                      return <Tag color="blue">{value}</Tag>;
-                    }
-                  },
-                },
-                {
-                  title: "Action",
-                  key: "operation",
-                  width: 100,
-                  render: () => (
-                    <div>
-                      <Dropdown menu={{ items: ReturnItems(row) }}>
-                        <MoreOutlined />
-                      </Dropdown>
-                    </div>
-                  ),
-                },
-              ]}
-              dataSource={[
-                {
-                  id: "#2387293",
-                  time: "5:00 PM",
-                  vehicleName: "Toyota Innova",
-                  vehicleNumber: "MH01 4656",
-                  status: "alloted",
-                },
-                {
-                  id: "#2387293",
-                  time: "5:00 PM",
-                  vehicleName: "Toyota Innova",
-                  vehicleNumber: "MH01 4656",
-                  status: "dispatched",
-                },
-                {
-                  id: "#2387293",
-                  time: "5:00 PM",
-                  vehicleName: "Toyota Innova",
-                  vehicleNumber: "MH01 4656",
-                  status: "completed",
-                },
-              ]}
-            />
-          </div>
-        ))}
+        <ViewLogs />
       </Drawer>
     </>
   );
