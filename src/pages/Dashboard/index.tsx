8126b968cc204aee81be221c290a051e4c5b8f0c
PrathamDogra <dogra.pratham@gmail.com>
2024-10-02 14:34:00 +0530

    Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,7 +3,18 @@ import cn from "classnames";
 import styles from "./index.module.scss";
 
 const Dashboard = () => {
-  return <div className={cn("container", styles.container)}>Dashboard</div>;
+  return (
+    <div className={cn("container", styles.container)}>
+      <div className={styles.headingContainer}>
+        <div>
+          <div className={styles.heading}>Dashboard</div>
+          <div className={styles.text}>
+            An overview of all company operations
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
